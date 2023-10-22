18d457bf4477a3b00e598db457a1317f84300948
Sudarev <soul7341@gmail.com>
2023-10-22 22:14:22 +0300

    Исправил недочеты и поправил проверку деления на 0

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,7 +6,10 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> divide = (x, y) -> x / y;
+    BinaryOperator<Integer> divide = (x, y) -> {
+        if (y == 0) return 0;
+        else return x / y;
+    };
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> ads = x -> x > 0 ? x : x * -1;
